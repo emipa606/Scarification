d3a75d2334dccfdb14e7f47d7b97aacda3ddf01d
emipa606 <emil.offermo@litet.net>
2020-06-30 23:14:48 +0200

    Added two traits for body-mod enthusiast/purist. Also compatible with the EM Tatoos and Piersings mod.

---
+++ Source/BodyModTraits/BodyModification.cs
@@ -0,0 +1,36 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace BodyModTraits
+{
+    class Internal
+    {
+        public static int countBodyMods(HediffSet hediffSet)
+        {
+            int num = 0;
+            List<Hediff> hediffs = hediffSet.hediffs;
+            for (int i = 0; i < hediffs.Count; i++)
+            {
+                if (hediffs[i].def.defName == "Tattooed" || hediffs[i].def.defName == "Scarified")
+                {
+                    num++;
+                    continue;
+                }
+                if (hediffs[i] is Hediff_Implant && hediffs[i].def.defName.StartsWith("EM") &&
+                    (hediffs[i].def.defName.EndsWith("Sma") || hediffs[i].def.defName.EndsWith("Med") || hediffs[i].def.defName.EndsWith("Big")))
+                {
+                    num++;
+                }
+            }
+            return num;
+        }
+    }
+
+    class BodyModificationEnthusiast : TraitDef
+    {
+    }
+    class BodyModificationPurist : TraitDef
+    {
+    }
+}
---
+++ Source/BodyModTraits/ThoughtWorker_HasAddedBodyMod.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using Verse;
+
+namespace BodyModTraits
+{
+    class ThoughtWorker_HasAddedBodyMod : ThoughtWorker
+    {		protected override ThoughtState CurrentStateInternal(Pawn p)
+		{
+			int num = Internal.countBodyMods(p.health.hediffSet);
+			if (num > 0)
+			{
+				return ThoughtState.ActiveAtStage(num - 1);
+			}
+			return false;
+		}
+	}
+}
