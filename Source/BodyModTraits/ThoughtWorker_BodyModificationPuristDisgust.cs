9b1a5544e1c981f72acc2d906adc1bb849e2c6d5
emipa606 <emil.offermo@litet.net>
2021-07-13 19:26:06 +0200

    Mod updated for 1.3 and passed autotests

--- Source/BodyModTraits/ThoughtWorker_BodyModificationPuristDisgust.cs
+++ Source/BodyModTraits/ThoughtWorker_BodyModificationPuristDisgust.cs
@@ -5,30 +5,34 @@ namespace BodyModTraits
 {
     public class ThoughtWorker_BodyModificationPuristDisgust : ThoughtWorker
     {
-
         protected override ThoughtState CurrentSocialStateInternal(Pawn p, Pawn other)
         {
             if (!p.RaceProps.Humanlike)
             {
                 return false;
             }
+
             if (!p.story.traits.HasTrait(TraitDef.Named("BodyModificationPurist")))
             {
                 return false;
             }
+
             if (!RelationsUtility.PawnsKnowEachOther(p, other))
             {
                 return false;
             }
+
             if (other.def != p.def)
             {
                 return false;
             }
-            int num = Internal.countBodyMods(other.health.hediffSet);
+
+            var num = Internal.countBodyMods(other.health.hediffSet);
             if (num > 0)
             {
                 return ThoughtState.ActiveAtStage(num - 1);
             }
+
             return false;
         }
     }
--- Source/BodyModTraits/ThoughtWorker_HasAddedBodyMod.cs
+++ Source/BodyModTraits/ThoughtWorker_HasAddedBodyMod.cs
@@ -3,15 +3,17 @@ using Verse;
 
 namespace BodyModTraits
 {
-    class ThoughtWorker_HasAddedBodyMod : ThoughtWorker
-    {		protected override ThoughtState CurrentStateInternal(Pawn p)
-		{
-			int num = Internal.countBodyMods(p.health.hediffSet);
-			if (num > 0)
-			{
-				return ThoughtState.ActiveAtStage(num - 1);
-			}
-			return false;
-		}
-	}
+    internal class ThoughtWorker_HasAddedBodyMod : ThoughtWorker
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            var num = Internal.countBodyMods(p.health.hediffSet);
+            if (num > 0)
+            {
+                return ThoughtState.ActiveAtStage(num - 1);
+            }
+
+            return false;
+        }
+    }
 }
